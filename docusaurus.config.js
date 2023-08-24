8b4c137478959644a13e003b06d277c40a941b22
Adithya Krishnan <krishsandeep@gmail.com>
2023-08-24 14:58:04 +0200

    Update favicon

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 const config = {
     title: "Workflow Automation in a Few Lines of Python",
     tagline: "Build Event-Trigger Based Automation Across Your SaaS Tools.",
-    favicon: "img/favicon.ico",
+    favicon: "favicon.ico",
 
     // Set the production url of your site here
     url: "https://querystar.io",
