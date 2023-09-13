0d959ba125a15de9b333e814b23a2539107b2328
Adithya Krishnan <krishsandeep@gmail.com>
2023-09-13 12:43:42 +0200

    Add: rag

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -66,6 +66,16 @@ const config = {
                 },
             };
         },
+        [
+            "@docusaurus/plugin-ideal-image",
+            {
+                quality: 70,
+                max: 1030, // max resized image's size.
+                min: 640, // min resized image's size. if original is lower, use that size.
+                steps: 2, // the max number of images generated between min and max (inclusive)
+                disableInDev: true, // disables plugin in dev mode
+            },
+        ],
     ],
 
     themeConfig:
@@ -201,6 +211,10 @@ const config = {
                             {
                                 label: "Knowledge Management",
                                 to: "/knowledge",
+                            },
+                            {
+                                label: "Retrieval Augmented Generation",
+                                to: "/rag",
                             },
                         ],
                     },
